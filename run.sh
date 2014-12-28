6fcab2ecd0c0e8d754f986eeef768e939b75da4a
David <david.medinets@gmail.com>
2014-12-28 14:31:08 -0500

    Add JSP views. Use spring-boot:run instead of java -jar

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar target/ragnvald-*.jar
+mvn package spring-boot:run
--- src/main/java/com/codebits/ragnvald/HelloController.java
+++ src/main/java/com/codebits/ragnvald/HelloController.java
@@ -1,20 +1,28 @@
 package com.codebits.ragnvald;
 
 import com.codebits.ragnvald.domain.PokomonSetRepository;
+import java.util.Date;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    
+
     @Autowired
     private PokomonSetRepository pokomonSetRepository = null;
 
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Map<String, Object> model) {
         System.out.println("count: " + pokomonSetRepository.count());
-        return "HelloWorld!";
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "welcome";
     }
 
 }
