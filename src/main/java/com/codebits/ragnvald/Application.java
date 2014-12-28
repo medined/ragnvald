81ca30667373c3f1149c6527f0fb3e44ec6e387f
David <david.medinets@gmail.com>
2014-12-27 19:09:37 -0500

    Switch to @SpringBootApplication

--- src/main/java/com/codebits/ragnvald/Application.java
+++ src/main/java/com/codebits/ragnvald/Application.java
@@ -1,14 +1,10 @@
 package com.codebits.ragnvald;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
